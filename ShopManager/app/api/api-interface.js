09f58409f97a1ce498f1723e50f19e328db2b86d
王劲鹏 <jingpeng.wang@outlook.com>
2017-06-19 02:33:04 +0800

    加入添加设备接口

--- ShopManager/app/api/api-interface.js
+++ ShopManager/app/api/api-interface.js
@@ -9,4 +9,11 @@ export default class APIInterface {
     formData.append("password", password)
     return BaseRequest.post(ApiConstant.BASE_URL + '/user/login', {}, formData)
   }
+
+  static deviceAdd(token, place, mac) {
+    let formData = new FormData();
+    formData.append("place", place)
+    formData.append("mac", mac)
+    return BaseRequest.post(ApiConstant.BASE_URL + '/device/add', {'token': token}, formData)
+  }
 }
