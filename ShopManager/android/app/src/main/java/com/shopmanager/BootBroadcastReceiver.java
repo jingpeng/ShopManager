c1093db75f38ae927bd97ef7576c44be9ca50d9c
王劲鹏 <jingpeng.wang@outlook.com>
2017-06-18 10:07:30 +0800

    完成App开机自启动

---
+++ ShopManager/android/app/src/main/java/com/shopmanager/BootBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package com.shopmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent appIntent = context.getPackageManager().getLaunchIntentForPackage("com.shopmanager");
+        context.startActivity(appIntent);
+    }
+}
