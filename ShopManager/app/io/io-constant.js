9533575a90daf1dca94f3c580d618ce6eeca8293
王劲鹏 <jingpeng.wang@outlook.com>
2017-06-30 08:46:28 +0800

    完成下载和自动播放机制

--- ShopManager/app/io/io-constant.js
+++ ShopManager/app/io/io-constant.js
@@ -1,4 +1,6 @@
 export default class IOConstant {
   static ADV_LIST = "advList"
   static ADV_LIST_ADMIN = "advListAdmin"
+
+  static ADV_DIRECTORY = '/shopmanager/advs/'
 }
--- ShopManager/app/screens/ad-screen.js
+++ ShopManager/app/screens/ad-screen.js
@@ -10,11 +10,16 @@ import {
 } from 'react-native'
 import { connect } from 'react-redux'
 import RNFS from "react-native-fs"
+import Video from 'react-native-video'
+import RCTDeviceEventEmitter from 'RCTDeviceEventEmitter'
 
 import ApiClient from '../api/api-client'
 import ApiInterface from '../api/api-interface'
 import ApiConstant from '../api/api-constant'
 import IOConstant from '../io/io-constant'
+
+const directory = RNFS.ExternalStorageDirectoryPath + IOConstant.ADV_DIRECTORY
+global.advs = []
 
 class AdScreen extends React.Component {
   static navigationOptions = {
@@ -23,17 +28,31 @@ class AdScreen extends React.Component {
 
   constructor(props) {
     super(props)
-
     this.state = {
       modalVisible: false,
+      imageVisible: false,
+      imageSource: '',
+      videoVisible: false,
+      videoSource: ''
     }
 
     this.getAdList.bind(this)
     this.downloadAds.bind(this)
+    this.autoPlayAds.bind(this)
+    this.dequeue.bind(this)
+    this.onLoad.bind(this)
   }
 
   componentDidMount() {
-    this.timer = setInterval(() => {
+    var copy = this
+    RCTDeviceEventEmitter.addListener('video_on_load', function(data) {
+      console.log(data)
+      copy.videoPlayTimer = setTimeout(() => {
+        copy.dequeue(advs)
+      }, data.duration)
+    })
+
+    this.downloadTimer = setInterval(() => {
       this.getAdList()
       .then(responses =>
         Promise.all(responses.map(response => response.json()))
@@ -44,12 +63,14 @@ class AdScreen extends React.Component {
       .catch((error) => {
         console.log(error);
       })
-      clearTimeout(this.timer)
+      clearTimeout(this.downloadTimer)
     }, 500)
   }
 
   componentWillUnmount() {
-    this.timer && clearTimeout(this.timer)
+    this.downloadTimer && clearTimeout(this.downloadTimer)
+    this.imagePlayTimer && clearTimeout(this.imagePlayTimer)
+    this.videoPlayTimer && clearTimeout(this.videoPlayTimer)
   }
 
   getAdList() {
@@ -73,6 +94,93 @@ class AdScreen extends React.Component {
       key: IOConstant.ADV_LIST_ADMIN,
       data: advsAdmin
     })
+
+    // 检查目录是否存在
+    RNFS.exists(directory).then((result) => {
+      if(!result) {
+        RNFS.mkdir(directory)
+      }
+    })
+
+    var futures = []
+    for (var i = 0; i < advs.length; i++) {
+      var adv = advs[i].advertisement
+      var path = directory + adv.id + '_' + adv.fileName
+      console.log(path)
+
+      futures.push(
+        RNFS
+        .downloadFile({
+          fromUrl: encodeURI(adv.fileSrc),
+          toFile: path,
+          background: false
+        })
+        .promise
+      )
+    }
+    // 合并下载结果
+    Promise.all(futures)
+    .then(responses => {
+      console.log(responses)
+      this.autoPlayAds()
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  autoPlayAds() {
+    storage.load({
+      key: IOConstant.ADV_LIST
+    }).then(result => {
+      result.reverse()
+      this.dequeue(result)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+  dequeue(result) {
+    var copy = this
+    var poped = result.pop()
+    console.log("=====" + poped + "=====")
+    if (!poped) {
+      return
+    }
+    var imageVisible
+    var videoVisible
+    var imageSource
+    var videoSource
+    var path = directory + poped.advertisement.id + '_' + poped.advertisement.fileName
+    switch (poped.advertisement.fileType) {
+      case 0:
+        imageVisible = true
+        videoVisible = false
+        advs = result
+        this.setState({
+          imageVisible: imageVisible,
+          imageSource: 'file://' + path,
+          videoVisible: videoVisible
+        })
+        this.imagePlayTimer = setTimeout(() => {
+          copy.dequeue(result)
+        }, poped.advertisement.time * 1000)
+        break
+      case 1:
+        imageVisible = false
+        videoVisible = true
+        advs = result
+        this.setState({
+          imageVisible: imageVisible,
+          videoVisible: videoVisible,
+          videoSource: path
+        })
+        break
+    }
+  }
+
+  onLoad(data) {
+    RCTDeviceEventEmitter.emit('video_on_load', data);
   }
 
   launchGame() {
@@ -88,12 +196,40 @@ class AdScreen extends React.Component {
   }
 
   render() {
+    let holder = null
+    if (this.state.imageVisible) {
+      holder =
+      <Image
+        style={styles.adBackground}
+        resizeMode={'contain'}
+        source={{uri: this.state.imageSource}}/>
+    }
+
+    if (this.state.videoVisible) {
+      holder =
+      <Video
+        ref={(ref) => { this.player = ref }}
+        source={{uri: this.state.videoSource}}   // Can be a URL or a local file.                                      // Store reference
+        rate={1.0}                              // 0 is paused, 1 is normal.
+        volume={1.0}                            // 0 is muted, 1 is normal.
+        muted={false}                           // Mutes the audio entirely.
+        paused={false}                          // Pauses playback entirely.
+        resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+        repeat={false}                           // Repeat forever.
+        playInBackground={true}                // Audio continues to play when app entering background.
+        // onLoadStart={this.loadStart}            // Callback when video starts to load
+        onLoad={this.onLoad}               // Callback when video loads
+        // onProgress={this.setTime}               // Callback every ~250ms with currentTime
+        // onEnd={this.onEnd}                      // Callback when playback finishes
+        // onError={this.videoError}               // Callback when video cannot be loaded
+        // onBuffer={this.onBuffer}                // Callback when remote video is buffering
+        // onTimedMetadata={this.onTimedMetadata}  // Callback when the stream receive some metadata
+        style={styles.backgroundVideo} />
+    }
+
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.adBackground}
-          source={require('../resources/ad-background.png')}/>
-
+        {holder}
         <View style={styles.trolleyContainer}>
           <TouchableNativeFeedback
             onPress={this.showBuyModal.bind(this)}>
@@ -136,7 +272,15 @@ const styles = StyleSheet.create({
   },
   adBackground: {
     height: Dimensions.get('window').height,
-    width: Dimensions.get('window').height * 1.6
+    width: Dimensions.get('window').width,
+    position: 'absolute'
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
   },
   trolleyContainer: {
     width: 90,
