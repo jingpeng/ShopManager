852def2bbeefe0dc9c9f016272b1049fba82d9ac
王劲鹏 <jingpeng.wang@outlook.com>
2017-07-22 21:17:15 +0800

    加入vortex pin库，点击key code透传

--- ShopManager/android/app/src/main/java/com/shopmanager/MainActivity.java
+++ ShopManager/android/app/src/main/java/com/shopmanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.shopmanager;
 
+import android.util.Log;
 import android.view.KeyEvent;
 
 import com.facebook.react.ReactActivity;
@@ -8,6 +9,8 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class MainActivity extends ReactActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -21,6 +24,8 @@ public class MainActivity extends ReactActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         WritableMap params = Arguments.createMap();
+        params.putInt("keyCode", keyCode);
+        Log.e(TAG, String.valueOf(keyCode));
         getReactInstanceManager().getCurrentReactContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit("on_key_pressed", params);
         return super.onKeyUp(keyCode, event);
--- ShopManager/android/app/src/main/java/com/shopmanager/MainApplication.java
+++ ShopManager/android/app/src/main/java/com/shopmanager/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.fileopener.FileOpenerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.rnfs.RNFSPackage;
+import com.vortex.pin.Pin;
 
 import java.util.Arrays;
 import java.util.List;
@@ -46,5 +47,10 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
+
+        //Pin.setFunc("PL10", Pin.FUNC_OUTPUT);
+        //Pin.setFunc("PL10", Pin.PULL_UP);
+        //Pin.setDlevel("PL10", Pin.Dlevel_3);
+        //Pin.setData("PL10", Pin.DATA_HIGH);
     }
 }
